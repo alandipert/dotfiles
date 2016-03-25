b072d4f2c69798745d84f7d2a9682d43bdcdd54d
Alan Dipert <alan@dipert.org>
2016-03-25 14:28:46 -0400

    adzerk: add cli tools etc to path

--- dotfiles/bash/adzerk.sh
+++ dotfiles/bash/adzerk.sh
@@ -1,3 +1,6 @@
 adzerk_env() {
   eval "$(gpg -d $HOME/Dropbox/dotfiles/.adzerk.asc)"
 }
+
+export ADZERK_SCRIPTS_PATH="${HOME}/github/adzerk/cli-tools/scripts"
+export PATH="${HOME}/github/adzerk/cli-tools:$PATH"
