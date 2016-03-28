d8a545cb40d4e6cbaff805e09a72e45371167e6a
Alan Dipert <alan@dipert.org>
2016-03-28 16:24:46 -0400

    conditionally set adzerk scripts path

--- dotfiles/bash/adzerk.sh
+++ dotfiles/bash/adzerk.sh
@@ -2,5 +2,7 @@ adzerk_env() {
   eval "$(gpg -d $HOME/Dropbox/dotfiles/.adzerk.asc)"
 }
 
-export ADZERK_SCRIPTS_PATH="${HOME}/github/adzerk/cli-tools/scripts"
+
+[[ -z "${ADZERK_SCRIPTS_PATH}" ]] && \
+  export ADZERK_SCRIPTS_PATH="${HOME}/github/adzerk/cli-tools/scripts"
 export PATH="${HOME}/github/adzerk/cli-tools:$PATH"
