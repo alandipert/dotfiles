9645142bf3988b5043c817d4a31656006bdac92c
Alan Dipert <alan@dipert.org>
2016-05-24 11:56:50 -0400

    add sum alias

--- dotfiles/bash/alias.sh
+++ dotfiles/bash/alias.sh
@@ -4,3 +4,4 @@ alias pbpaste='xsel --clipboard --output'
 alias ll='ls --color=auto -Flh'
 alias e='emacsclient -nq'
 alias st='stterm -f terminus:pixelsize=16:antialias=false:autohint=false'
+alias sum="perl -nle '\$sum += \$_ } END { print \$sum'"
