de851a3e0841ae8b795f6707bd392745974e1560
Alan Dipert <alan@dipert.org>
2016-08-29 09:25:31 -0400

    emacs whatnots

--- dotfiles/bash/alias.sh
+++ dotfiles/bash/alias.sh
@@ -2,6 +2,5 @@ alias g=git
 alias pbcopy='xsel --clipboard --input'
 alias pbpaste='xsel --clipboard --output'
 alias ll='ls --color=auto -Flh'
-alias e='emacsclient -nq'
 alias st='stterm -f terminus:pixelsize=16:antialias=false:autohint=false'
 alias sum="perl -nle '\$sum += \$_ } END { print \$sum'"
