ef230acc38a9633d6bee921a9aee1ba7a14b69b8
Alan Dipert <alan@dipert.org>
2016-11-16 10:11:07 -0800

    volume improvements

--- dotfiles/bash/volume.sh
+++ dotfiles/bash/volume.sh
@@ -3,16 +3,8 @@
 # volume.sh # mute
 # volume.sh 5+ # increase by 5%
 # volume.sh 5- # decrease by 5%
-set -e
 if [ -z "$1" ]; then
   amixer -q -D pulse sset Master toggle
-  if $(amixer sget Master|tail -1|grep on); then
-    notify-send "Unmuted"
-  else
-    notify-send "Muted"
-  fi
 else
   amixer -q sset Master,0 $1 unmute
-  VOLUME=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master))
-  notify-send "Volume: $VOLUME"
 fi
