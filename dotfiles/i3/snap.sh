dc938d8d2812781cb6342b731bab2fa82b42e4f6
Alan Dipert <alan@dipert.org>
2016-11-27 15:38:49 -0800

    dont print screenshot url with newline

--- dotfiles/i3/snap.sh
+++ dotfiles/i3/snap.sh
@@ -6,5 +6,5 @@ DB_BASE_URL="https://dl.dropboxusercontent.com/u/${USER_ID}/Screenshots"
 IMG="$(uuidgen).png"
 scrot -s "${OUTDIR}/${IMG}"
 URL="${DB_BASE_URL}/${IMG}"
-echo $URL | xsel --clipboard --input
+echo -n $URL | xsel --clipboard --input
 notify-send "Copied link to clipboard"
