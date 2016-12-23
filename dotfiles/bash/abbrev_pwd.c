d702c9854029f059e5cada8615bb59604bf46c47
Alan Dipert <alan@dipert.org>
2016-12-23 00:36:29 -0800

    Use mkgo for prompt generation

---
+++ dotfiles/bash/abbrev_pwd.c
@@ -0,0 +1,53 @@
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/* If prefix is found at beginning of str, return length of prefix. */
+int startswith(char *str, char *prefix) {
+  for(int i = 0;; i++) {
+    if(str[i] == '\0' || prefix[i] == '\0') {
+      return i;
+    } else if(str[i] != prefix[i]) {
+      return 0;
+    }
+  }
+}
+
+void print_abbrev(char *home, char *cwd) {
+  int after_home = startswith(cwd, home);
+  char *cwd_tail;
+
+  if(after_home != 0) {
+    printf("~");
+  }
+
+  cwd = cwd+after_home;
+  if(cwd[0] == '\0') {
+    printf("\n");
+    return;
+  }
+
+  cwd_tail = ++cwd;
+  while(cwd_tail = strchr(cwd, '/'), cwd_tail != NULL) {
+    printf("/%.*s", 1, cwd);
+    cwd = cwd_tail+1;
+  }
+
+  printf("/%s", cwd);
+}
+
+int main(int argc, char **argv) {
+  struct passwd *passwd_ent = getpwuid(getuid());
+  char *home                = passwd_ent->pw_dir;
+  char *line                = NULL;
+  int len;
+  size_t size;
+  while(len = getline(&line, &size, stdin), len != -1) {
+    line[len-1] = '\0';         /* Trim newline */
+    print_abbrev(home, line);
+    free(line);
+  }
+  return 0;
+}
