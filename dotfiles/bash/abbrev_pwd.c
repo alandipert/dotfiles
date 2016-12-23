158dc712da282c5d8cccbba325578c0e7c736a68
Alan Dipert <alan@dipert.org>
2016-12-23 01:26:47 -0800

    done shaving

--- dotfiles/bash/abbrev_pwd.c
+++ dotfiles/bash/abbrev_pwd.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <unistd.h>
 
-/* If prefix is found at beginning of str, return length of prefix. */
 int startswith(char *str, char *prefix) {
   for(int i = 0;;i++) {
     if(prefix[i] == '\0') return i;
@@ -16,17 +15,11 @@ void print_abbrev(char *home, char *cwd) {
   int after_home = startswith(cwd, home);
   char *cwd_tail;
 
-  if(after_home != 0) {
-    printf("~");
-  }
+  if(after_home != 0) printf("~");
 
-  cwd = cwd+after_home;
-  if(cwd[0] == '\0') {
-    printf("\n");
-    return;
-  }
+  if(cwd[after_home] == '\0') return;
 
-  cwd_tail = ++cwd;
+  cwd_tail = cwd = cwd+after_home+1;
   while(cwd_tail = strchr(cwd, '/'), cwd_tail != NULL) {
     printf("/%.*s", 1, cwd);
     cwd = cwd_tail+1;
@@ -38,13 +31,8 @@ void print_abbrev(char *home, char *cwd) {
 int main(int argc, char **argv) {
   struct passwd *passwd_ent = getpwuid(getuid());
   char *home                = passwd_ent->pw_dir;
-  char *line                = NULL;
-  int len;
-  size_t size;
-  while(len = getline(&line, &size, stdin), len != -1) {
-    line[len-1] = '\0';         /* Trim newline */
-    print_abbrev(home, line);
-    free(line);
-  }
+  char *cwd                 = getcwd(NULL, 0);
+  print_abbrev(home, cwd);
+  free(cwd);
   return 0;
 }
