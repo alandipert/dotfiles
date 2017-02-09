134c9f8fdc410c190b718727b499c0ead1739d5f
Alan Dipert <alan@dipert.org>
2017-02-09 15:46:37 -0800

    Use local mkgo

--- dotfiles/bash/reset_kb.sh
+++ dotfiles/bash/reset_kb.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo mkgo ~/.bash/usbreset.c $(lsusb|awk '$6 == "17ef:6047" { printf("/dev/bus/usb/%s/%.3s\n", $2, $4) }')
+sudo ~/.bash/mkgo ~/.bash/usbreset.c $(lsusb|awk '$6 == "17ef:6047" { printf("/dev/bus/usb/%s/%.3s\n", $2, $4) }')
