db6ab3d0e9727076e2038042fc2f1bcc8b041307
Alan Dipert <alan@dipert.org>
2017-02-15 20:24:54 -0800

    brightness

--- dotfiles/i3/brightness.sh
+++ dotfiles/i3/brightness.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-BRIGHTNESS="$(xrandr --verbose|grep Brightness|awk '{print $2}'|tr -d 0.)"
-NEW_BRIGHTNESS="$(zenity --entry --title Brightness --entry-text=${BRIGHTNESS} --text='New brightness (1-10):')"
-if [[ "$NEW_BRIGHTNESS" == "10" ]]; then
+BRIGHTNESS="$(xrandr --verbose|grep Brightness|awk '{print $2}')"
+
+case $1 in
+  up) ADJUSTMENT="+0.1" ;;
+  down) ADJUSTMENT="-0.1" ;;
+  *) exit 1 ;;
+esac
+
+NEW_BRIGHTNESS="$(echo ${BRIGHTNESS}${ADJUSTMENT}|bc)"
+
+if [[ $(echo "$NEW_BRIGHTNESS <= 0"|bc) == 1 ]]; then
+  xrandr --output DP1 --brightness 0
+elif [[ $(echo "$NEW_BRIGHTNESS >= 1"|bc) == 1 ]]; then
   xrandr --output DP1 --brightness 1
 else
-  xrandr --output DP1 --brightness "0.${NEW_BRIGHTNESS}"
+  xrandr --output DP1 --brightness $NEW_BRIGHTNESS
 fi
-
