9104d02e7e93cdc5c65cd96e1fec6d186e25c894
Alan Dipert <alan@dipert.org>
2017-04-29 19:57:58 -0700

    ll for MAc

--- dotfiles/bash/alias.sh
+++ dotfiles/bash/alias.sh
@@ -1,6 +1,11 @@
 alias g=git
-alias pbcopy='xsel --clipboard --input'
-alias pbpaste='xsel --clipboard --output'
-alias ll='ls --color=auto -Flh'
 alias st='stterm -f terminus:pixelsize=16:antialias=false:autohint=false'
 alias sum="perl -nle '\$sum += \$_ } END { print \$sum'"
+
+if [[ $(uname -s) == "Darwin" ]]; then
+  alias ll='gls --color=auto -Flh'
+else
+  alias pbcopy='xsel --clipboard --input'
+  alias pbpaste='xsel --clipboard --output'
+  alias ll='ls --color=auto -Flh'
+fi
